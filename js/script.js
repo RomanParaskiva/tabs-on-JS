1309f6b67408a68c2729577909c5c3454f75affa
Roman <parasta2018@gmail.com>
2019-10-08 22:03:31 +0300

    start timer

--- js/script.js
+++ js/script.js
@@ -32,7 +32,66 @@ window.addEventListener('DOMContentLoaded', function(){
             }
         }
     });
+
+//таймер
+
+let deadLine = "2019-10-9";
+
+function getTimeRemaining(endtime) {
+    let t  = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = 0,
+        minutes = 0,
+        hours = 0;
+
+    if (Date.parse(endtime) > Date.parse(new Date())) {
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60),
+        hours = Math.floor((t/(1000*60*60)));
+        // days = Math.floor((t/(1000*60*60*24)));
+    } 
+    return {
+        'total' :  t,
+        'hours' : hours,
+        'minutes' : minutes,
+        'seconds' : seconds
+    }
+}
+
+function setClock (id, endtime) {
+    let timer = document.getElementById(id),
+        hours = timer.querySelector(".hours"),
+        minutes = timer.querySelector(".minutes"),
+        seconds = timer.querySelector(".seconds"),
+        timeInterval = setInterval(updateClock, 1000);
+
+    function updateClock () {
+        let t = getTimeRemaining(deadLine);
+        if (t.hours < 10) {
+            hours.textContent = "0" + t.hours;
+        } else {
+            hours.textContent = t.hours;
+        }
+        if (t.minutes < 10) {
+            minutes.textContent = "0" + t.minutes;
+        } else {
+            minutes.textContent = t.minutes;
+        }
+        if (t.seconds < 10) {
+            seconds.textContent = "0" + t.seconds;
+        } else {
+            seconds.textContent = t.seconds;
+        }
+        
+        if(t.total <= 0) {
+            clearInterval(timeInterval);
+        }
+    }
+};
+
+setClock('timer', deadLine);
 });
+
+
 
     
 
